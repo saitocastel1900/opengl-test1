8a5f2e864b7aca3dea5a7d9b474a1ea5f62377fa
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-08-15 13:55:49 +0900

    背景の描画処理を追加しました

--- ソース.cpp
+++ ソース.cpp
@@ -1,11 +1,11 @@
+#include <cstdlib>
 #include<iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 using namespace std;
 
 int main()
 {
-	cout <<"Hello World"<<endl;
-
 	//glfwInitを使用することでOpenGLを使う準備が行われる
 	if (glfwInit() == GL_FALSE)
 	{
@@ -13,8 +13,40 @@ int main()
 		return 1;
 	}
 
-	GLFWwindow* const window(glfwCreateWindow(640, 480, "Hello!", NULL, NULL));
+	//ウィンドウを作成する
+	GLFWwindow* const window(glfwCreateWindow(640, 480, "HelloWolrd", NULL, NULL));
+	if (window==NULL)
+	{
+		//ウィンドウが作成出来なかった
+		cerr << "ウィンドウの作成が出来ませんでした" << endl;
+		return 1;
+	}
+
+	//プログラム終了時に必ずglfwTerminateが実行される
+	//終了直前にglfwTerminateを実行する必要があるが、すべてに追加するのがめんどいためatexitで登録
+	//glfwTerminateは終了処理を行う関数(初期化前に戻す)
+	atexit(glfwTerminate);
+
+	//作成したウィンドウをOpenGLの処理対象にする
 	glfwMakeContextCurrent(window);
+
+	//背景色の設定（マゼンタ,RGBA）
+	glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
+
+	//ループすることで、ウィンドウを閉じないようにしている
+	while (glfwWindowShouldClose(window) == GL_FALSE)
+	{
+		// ウィンドウを消去する(フレームバッファのカラーバッファ)
+		//ウィンドウの塗りつぶし
+		glClear(GL_COLOR_BUFFER_BIT);
+		//
+		// ここで描画処理を行う
+		//
+		// カラーバッファを入れ替える（ダブルバッファリング:ちらつきを防ぐ）
+		glfwSwapBuffers(window);
+		// イベントを取り出す
+		glfwWaitEvents();
+	}
 
 	return 0;
 }
