7410c17cef781c6e735005d87151f1cc0c7efd66
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-08-15 12:27:06 +0900

    window表示機能の追加

--- ソース.cpp
+++ ソース.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include <GLFW/glfw3.h>
+using namespace std;
+
 int main()
 {
+	cout <<"Hello World"<<endl;
+
+	//glfwInitを使用することでOpenGLを使う準備が行われる
+	if (glfwInit() == GL_FALSE)
+	{
+		cerr << "初期化に失敗した" << endl;
+		return 1;
+	}
+
+	GLFWwindow* const window(glfwCreateWindow(640, 480, "Hello!", NULL, NULL));
+	glfwMakeContextCurrent(window);
 
 	return 0;
 }
